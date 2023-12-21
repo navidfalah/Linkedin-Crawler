8170e198dfd4a9fecbcf993016c2b614b7995cb4
navid <navid.falah7@gmail.com>
2023-12-21 21:30:03 +0330

    logginer and searcher added

--- crawler.py
+++ crawler.py
@@ -1,87 +1,91 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 from constants import email, password
 import re
 from bs4 import BeautifulSoup
-import json
+import csv
+from time import sleep
+
 
 name_researcher = "Behnam Yousefi"
 
-# Initialize the WebDriver
-driver = webdriver.Chrome()  # Or whichever browser you're using
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
 
-driver.get("https://www.linkedin.com/login")
-username = driver.find_element(By.ID, "username")
-username.send_keys(email)
-password_input = driver.find_element(By.ID, 'password')
-password_input.clear()
-password_input.send_keys(password)
+def loginner():
+    driver.get("https://www.linkedin.com/login")
+    username = wait.until(EC.visibility_of_element_located((By.ID, "username")))
+    username.send_keys(email)
+    
+    password_input = wait.until(EC.visibility_of_element_located((By.ID, 'password')))
+    password_input.clear()
+    password_input.send_keys(password)
 
-sleep(3)
+    sign_in_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Sign in']")))
+    sign_in_button.click()
 
-sign_in_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Sign in']"))
-)
-sign_in_button.click()
+def searcher(name_researcher):
+    driver.get(f"https://www.linkedin.com/search/results/people/?keywords={name_researcher}&origin=GLOBAL_SEARCH_HEADER&sid=GGF")
+    first_li = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "li.reusable-search__result-container")))
+    first_link = first_li.find_element(By.TAG_NAME, "a")
+    first_link.click()
 
-# Wait for login to complete
-sleep(5)
+    sleep(3)
+
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    extracted_data = []
+    li_tags = soup.find_all('li', class_='artdeco-list__item')
+    for li in li_tags:
+        a_tag = li.find('a', {'data-field': 'experience_company_logo'})
+        img_tag = li.find('div', class_='ivm-image-view-model').find('img') if li.find('div', class_='ivm-image-view-model') else None
+        if img_tag:
+            image_alt = img_tag.get('alt', '')
+            image_alt = re.sub(r'\s+logo$|\\u[0-9a-fA-F]{4}|[0-9]+', '', image_alt, flags=re.IGNORECASE)
+            link = a_tag.get('href', '') if a_tag else ''
+            if link:
+                data = {
+                    'link': link,
+                    'image_src': img_tag.get('src', ''),
+                    'image_alt': image_alt,
+                }
+                extracted_data.append(data)
+    return extracted_data
+
+data_dict = {}
 
 
-driver.get(f"https://www.linkedin.com/search/results/people/?keywords={name_researcher}&origin=GLOBAL_SEARCH_HEADER&sid=GGF")
+with open('sample_1000 - Sheet1.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    data = list(reader)
 
-sleep(5)
-# Find the first `li` tag
-first_li = driver.find_element(By.CSS_SELECTOR, "li.reusable-search__result-container")
+for row in data:
+    if row:
+        latin_name = row[3]
+        family_name = row[4]
+        key = row[0]
+        value = f"{latin_name} {family_name}"
+        data_dict[key] = value
 
-# Find the first link within the `li` tag
-first_link = first_li.find_element(By.TAG_NAME, "a")
-
-# Click on the link
-first_link.click()
-
-sleep(5)
+loginner()
 
 
-# Use JavaScript to select all content, copy it to a hidden textarea, and return its value
-page_source = driver.page_source
+for id_name, name_value in data_dict.items():
+  
+    try:
+        extracted_data = searcher(name_value)
+        for row in data:
+            if row[0] == id_name:
+                row.append(extracted_data)
+                break
+    except:
+        print("error")
 
-soup = BeautifulSoup(page_source, 'html.parser')
 
-# Define the structure to hold extracted data
-extracted_data = []
-
-# Find all <li> tags with the specified class
-li_tags = soup.find_all('li', class_='artdeco-list__item')
-
-for li in li_tags:
-    # Extract the href attribute from <a> tags and the src attribute from <img> tags within the 'ivm-image-view-model' class
-    a_tag = li.find('a', {'data-field': 'experience_company_logo'})
-    img_tag = li.find('div', class_='ivm-image-view-model').find('img') if li.find('div', class_='ivm-image-view-model') else None
-
-    if img_tag:
-        image_alt = img_tag.get('alt', '')
-
-        # Remove 'logo' if it is at the end of the image_alt text
-        # image_alt = re.sub(r'\s+logo$', '', image_alt, flags=re.IGNORECASE)
-        image_alt = re.sub(r'\s+logo$|\\u[0-9a-fA-F]{4}|[0-9]+', '', image_alt, flags=re.IGNORECASE)
-
-        link = a_tag.get('href', '') if a_tag else ''
-        if link:  # Check if link is not empty
-            data = {
-                'link': link,
-                'image_src': img_tag.get('src', ''),
-                'image_alt': image_alt,
-            }
-
-            extracted_data.append(data)
-
-# Write the extracted content to a new file in JSON format
-with open('extracted_content.json', 'w', encoding='utf-8') as file:
-    json.dump(extracted_data, file, indent=4)
-
+with open('sample_1000 - Sheet1_updated.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
 
 driver.quit()
